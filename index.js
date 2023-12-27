8842d10b13738c27bbba7a97193543dd5f6859a4
ashley773877 <mirandaashley16@gmail.com>
2023-12-27 18:55:35 -0500

    second part started

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26\n"
 let currentCell = 1
 // instead of inidividual variables for each cell, i used aarray "cells" to make the value
-let cells = ['', '', '', ''];
+let cells = [];
+let numColumns = 0;
 
 
 
@@ -18,4 +19,7 @@ for(let i = 0; i < csv.lengths; i++) {
     cells[currentCell - 1] += csv[i];
 }
 }
+
+//part 2
+
 
